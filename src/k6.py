bfb18bf77b8288e2d08137f7606887bd45701476
Luca Bello <luca.bello@canonical.com>
2025-03-17 17:41:57 +0100

    push some progress

--- src/k6.py
+++ src/k6.py
@@ -10,6 +10,8 @@ import re
 from ops.pebble import Layer
 import logging
 from ops import Application, Unit
+import socket
+import requests
 
 logger = logging.getLogger(__name__)
 
@@ -18,7 +20,19 @@ class K6Status(Enum):
     """Helper class to represent the status of k6 units."""
 
     idle = "idle"  # ready to accept a new test
-    active = "active"  # currently executing `k6 run`
+    busy = "busy"  # currently executing `k6 run` (even if paused)
+
+
+class K6Api:
+    """Helper class to interact with the k6 HTTP API."""
+
+    @staticmethod
+    def resume(endpoint: str) -> None:
+        """Resume a paused load test."""
+        k6_resume_payload = {"data": {"attributes": {"paused": True}}}
+        response = requests.patch(endpoint, json=k6_resume_payload)
+        if response.status_code != 200:
+            raise Exception(f"Cannot start test on {endpoint}: received {response.status_code}")
 
 
 class K6(ops.Object):
@@ -61,7 +75,8 @@ class K6(ops.Object):
                 data[key] = json.loads(str(value))
         return data if data else None
 
-    def get_all_peer_data(self) -> Optional[Dict]:
+    def get_all_peer_unit_data(self) -> Optional[Dict]:
+        """Get data from the peer relation for all units."""
         if not self.peers:
             return None
         data = {}
@@ -80,7 +95,7 @@ class K6(ops.Object):
                         "override": "replace",
                         "summary": "k6 service",
                         "command": f"/usr/bin/k6 run {script_path} --vus {vus} --paused",
-                        "startup": "disabled",
+                        "startup": "enabled",
                         "environment": {
                             "https_proxy": os.environ.get("JUJU_CHARM_HTTPS_PROXY", ""),
                             "http_proxy": os.environ.get("JUJU_CHARM_HTTP_PROXY", ""),
@@ -103,10 +118,10 @@ class K6(ops.Object):
             except ops.pebble.APIError:
                 logger.info("k6 is not running")
             return
-        # Else, set it and replan
+        # Else, set it, replan and start the load tests from the leader
         self.container.add_layer(self._layer_name, Layer(layer_dict), combine=True)
         self.container.replan()
-        self.container.start(self._service_name)
+        self._start_if_ready()
 
     def _get_vus_from_script(self, script_path: str) -> int:
         """Extract the VUs from a script."""
@@ -123,35 +138,55 @@ class K6(ops.Object):
         """Set 'idle' status in each unit if they have no other status."""
         data = self.get_peer_data(self._charm.unit)
         if not data or "status" not in data:
-            # TODO: add unit fqdn to peer data for /start call
-            self.set_peer_data(self._charm.unit, {"status": K6Status.idle})
+            self.set_peer_data(
+                self._charm.unit, {"status": K6Status.idle, "endpoint": socket.getfqdn()}
+            )
 
-    def _start(self):
-        """Aaa."""
-        if not self._charm.unit.is_leader():
+    def _start_if_ready(self):
+        """Start a k6 load test in all units."""
+        if (
+            not self._charm.unit.is_leader()
+            or not self.peers
+            or not (peer_data := self.get_all_peer_unit_data())
+            or not self.are_all_units_ready()
+        ):
             return
-        data = {}
-        for unit in self.peers.units:
-            pass  # TODO: below
-        # Get all the peer unit databags
-        # checks for paused in all of them
-        # tenacity-retry this method if they are not all "paused" (active)
-        # if yes, get their IPs and /start the tests on each one
+        endpoints = [unit_data.get("endpoint") for unit_data in peer_data.values()]
+        for endpoint in endpoints:
+            K6Api.resume(endpoint=endpoint)
 
     def run(self, *, script_path: str):
         """Set a command in the Pebble layer for all units."""
         vus: int = self._get_vus_from_script(script_path=script_path)
         layer = self._pebble_layer(script_path=script_path, vus=vus)
         self.set_peer_data(
-            self._charm.app, data={"layer": layer.to_dict(), "status": K6Status.active}
+            self._charm.app, data={"layer": layer.to_dict(), "status": K6Status.busy}
         )
 
     def stop(self):
         """Stop `k6` in all the units."""
-        self.set_peer_data(self._charm.app, data={})
+        self.set_peer_data(self._charm.app, data={"layer": {}, "status": K6Status.idle})
+
+    def are_all_units_ready(self) -> bool:
+        """Check whether all k6 units are ready for tests to be started.
+
+        This means that the Pebble layer has been set with the (--paused) flag,
+        and that all the units set their status as "busy" in peer data.
+        """
+        peer_data = self.get_all_peer_unit_data()
+        if not peer_data:
+            return False
+        return all(unit_data.get("status") == K6Status.busy for unit_data in peer_data)
 
     def is_running(self) -> bool:
-        """Check whether k6 is currently running."""
+        """Check whether k6 is currently running in any unit."""
+        peer_data = self.get_all_peer_unit_data()
+        if not peer_data:
+            return False
+        return any(unit_data.get("status") == K6Status.busy for unit_data in peer_data)
+
+    def is_running_on_unit(self) -> bool:
+        """Check whether k6 is currently running in the current unit."""
         try:
             service = self.container.get_service(self._service_name)
             if not service:
