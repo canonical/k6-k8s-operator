23efd27c69926680cacee17a577e1e96d5c08101
Luca Bello <luca.bello@canonical.com>
2026-04-17 10:43:26 +0200

    remove unused files

--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright 2025 Canonical Ltd.
-# See LICENSE file for licensing details.
-"""Tests for the k6-k8s charm."""
--- tests/unit/__init__.py
+++
@@ -1,3 +0,0 @@
-# Copyright 2025 Canonical Ltd.
-# See LICENSE file for licensing details.
-"""Unit tests for the k6-k8s charm."""
